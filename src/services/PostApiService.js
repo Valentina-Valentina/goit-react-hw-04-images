dec583a92bc7e905bd4b6b4faec84238b8ec3983
Valentina-Valentina <128928850+Valentina-Valentina@users.noreply.github.com>
2023-10-22 07:19:50 +0300

    Initial commit

---
+++ src/services/PostApiService.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '39116892-5a61e1a38d49639946fc5021c';
+
+export default class PostsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHits = 0;
+  }
+
+  async fetchPost() {
+    const OPTIONS = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: 12,
+    });
+
+    const response = await axios.get(`${BASE_URL}?${OPTIONS.toString()}`);
+    this.incrementPage();
+    
+    return response.data;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get hits() {
+    return this.totalHits;
+  }
+
+  set hits(newTotalHits) {
+    this.totalHits = newTotalHits;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
